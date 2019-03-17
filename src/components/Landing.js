4d37a7ccc6068e38041601904a21b924bf28cc7e
JoePascale <joe.m.pascale@gmail.com>
2019-03-17 10:40:26 -0700

    added basic landing content

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 const Landing = () => (
   <section className="landing">
-    Landing page will go here
+    <h1 className="hero-title">Turn it up!</h1>
+
+    <section className="selling-points">
+      <div className="point">
+        <h2 className="point-title">Choose your music</h2>
+        <p className="point-description">Personalize your playlist. Make it your own.</p>
+      </div>
+      <div className="point">
+        <h2 className="point-title">Unlimited, streaming, ad-free</h2>
+        <p className="point-description">No limits. No distractions.</p>
+      </div>
+      <div className="point">
+        <h2 className="point-title">Mobile enabled</h2>
+        <p className="point-description">Listen to your music on the go.</p>
+      </div>
+    </section>
+
   </section>
 );
 
