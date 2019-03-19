9842c9d2a7de09d9c60b19a22888f74a99bce824
JoePascale <joe.m.pascale@gmail.com>
2019-03-18 22:21:56 -0700

    added album listings to Libary page

---
+++ src/data/albums.js
@@ -0,0 +1,41 @@
+export default [{
+  title: 'Not the Actual Events',
+  artist: 'Nine Inch Nails',
+  releaseInfo: '2016 The Null Corporation',
+  albumCover: '/assets/images/album_covers/Not_the_Actual_Events.jpg',
+  slug: 'not-the-actual-events',
+  songs: [
+      { title: 'Branches/Bones', duration: '107', audioSrc: '' },
+      { title: 'Dear World', duration: '247', audioSrc: '' },
+      { title: 'Shes Gone Away', duration: '360', audioSrc: '' },
+      { title: 'The Idea of You', duration: '207', audioSrc: '' },
+      { title: 'Burning Bright (Field on Fire)', duration: '350', audioSrc: '' }
+  ]
+}, {
+    title: 'Add Violence',
+    artist: 'Nine Inch Nails',
+    releaseInfo: '2017 The Null Corporation',
+    albumCover: '/assets/images/album_covers/Add_Violence.jpg',
+    slug: 'add-violence',
+    songs: [
+      { title: 'Less Than', duration: '', audioSrc: '/assets/music/01 - Nine Inch Nails - LESS THAN.mp3' },
+      { title: 'The Lovers', duration: '', audioSrc: '/assets/music/02 - Nine Inch Nails - THE LOVERS.mp3' },
+      { title: 'This Isnt the Place', duration: '', audioSrc: '/assets/music/03 - Nine Inch Nails - THIS ISNT THE PLACE.mp3' },
+      { title: 'Not Anymore', duration: '', audioSrc: '/assets/music/04 - Nine Inch Nails - NOT ANYMORE.mp3' },
+      { title: 'The Background World', duration: '', audioSrc: '/assets/music/05 - Nine Inch Nails - THE BACKGROUND WORLD.mp3' }
+    ]
+}, {
+    title: 'Bad Witch',
+    artist: 'Nine Inch Nails',
+    releaseInfo: '2018 The Null Corporation',
+    albumCover: '/assets/images/album_covers/Bad_Witch.jpg',
+    slug: 'bad-witch',
+    songs: [
+      { title: 'Shit Mirror', duration: '', audioSrc: '/assets/music/01 - Nine Inch Nails - LESS THAN.mp3' },
+      { title: 'Ahead of Ourselves', duration: '', audioSrc: '/assets/music/02 - Nine Inch Nails - THE LOVERS.mp3' },
+      { title: 'Play the Goddamned Part', duration: '', audioSrc: '/assets/music/03 - Nine Inch Nails - THIS ISNT THE PLACE.mp3' },
+      { title: 'God Break Down the Door', duration: '', audioSrc: '/assets/music/04 - Nine Inch Nails - NOT ANYMORE.mp3' },
+      { title: 'Im Not from this World', duration: '', audioSrc: '/assets/music/05 - Nine Inch Nails - THE BACKGROUND WORLD.mp3' },
+      { title: 'Over and Out', duration: '', audioSrc: '/assets/music/05 - Nine Inch Nails - THE BACKGROUND WORLD.mp3' }
+    ]
+}];
