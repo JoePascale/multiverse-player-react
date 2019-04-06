e7f9967283aa5ffcb00b24e517ae6f9e8d199dae
JoePascale <joe.m.pascale@gmail.com>
2019-04-06 10:13:56 -0700

    added player bar component

---
+++ src/components/PlayerBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PlayerBar extends Component {
+  render() {
+    return (
+      <section className="player-bar">
+        This is where the super cool player bar will go
+      </section>
+    );
+  }
+}
+
+export default PlayerBar;
