593ddea5c44f51658861d33d9f2f5f1ede6f8ecc
JoePascale <joe.m.pascale@gmail.com>
2019-04-07 19:50:45 -0700

    next button

--- src/components/Album.js
+++ src/components/Album.js
@@ -54,6 +54,14 @@ class Album extends Component {
     this.play();
   }
 
+  handleNextClick() {
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   convertTime(duration) {
     var minutes = Math.floor(duration/60);
     var seconds = duration - (minutes*60);
@@ -124,6 +132,7 @@ class Album extends Component {
           currentSong={this.state.currentSong}
           handleSongClick={() => this.handleSongClick(this.state.currentSong)}
           handlePrevClick={() => this.handlePrevClick()}
+          handleNextClick={() => this.handleNextClick()}
         />
       </section>
     );
